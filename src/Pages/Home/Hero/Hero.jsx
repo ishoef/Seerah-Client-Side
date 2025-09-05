84b7b1e8f9d2ad0102a44c1e0ebbf544cd2205a7
ishoef <ismailhossennayeb@gmail.com>
2025-09-05 10:02:31 +0600

    Feat: Responsiveness implement in hero section

--- src/Pages/Home/Hero/Hero.jsx
+++ src/Pages/Home/Hero/Hero.jsx
@@ -9,7 +9,7 @@ export default function Hero() {
       overflow-hidden
       [background-image:linear-gradient(to_right,rgba(229,231,235,0.3)_1px,transparent_1px),linear-gradient(to_bottom,rgba(229,231,235,0.3)_1px,transparent_1px)] 
       dark:[background-image:linear-gradient(to_right,rgba(75,85,99,0.3)_1px,transparent_1px),linear-gradient(to_bottom,rgba(75,85,99,0.3)_1px,transparent_1px)]
-      [background-size:20px_20px] border-b border-b-gray-400 pt-20"
+      [background-size:20px_20px] border-b border-b-gray-400 mt-5 md:pt-20"
     >
       {/* Gradient Overlay */}
       <div className="absolute inset-0 bg-gradient-to-br from-blue-100 via-transparent to-purple-100 dark:from-blue-900/40 dark:via-gray-900 dark:to-purple-900/40 pointer-events-none" />
